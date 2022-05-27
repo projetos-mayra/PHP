0793fef37f49fdf9ad2a9547dbd0f62ab8241e88
Mayra Bruno <80061821+projetos-mayra@users.noreply.github.com>
2022-05-27 12:39:38 -0300

    Add files via upload

---
+++ processa01.php
@@ -0,0 +1,19 @@
+<?php
+//4- Função com retorno e com paramentros
+
+
+
+function teste4($x,$y){
+    $soma = $x + $y;
+    return $soma;
+
+}
+
+$resultado = teste4 (10,20);
+echo " A soma é: $resultado";
+
+
+echo"<hr>";
+
+
+?>
